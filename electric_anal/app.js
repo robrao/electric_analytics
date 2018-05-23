3e86ec2e2b093c7d7246c5df635644a32e7659af
robrao <rob.rao@arterys.com>
2018-05-23 16:08:49 -0600

    attempting to access db from app.js, remove index.html, using ejs to insert data into html

--- electric_anal/app.js
+++ electric_anal/app.js
@@ -6,14 +6,24 @@ const app = express();
 
 let db;
 
+app.set('view engine', 'ejs');
 app.use('/assets', express.static(`${__dirname}/public`));
 app.use('/node_modules', express.static(`${__dirname}/node_modules`));
+
+app.get('/my_db', (req, res) => {
+    console.warn(`here here here`);
+    db.collection('electric_analytics').find({}).toArray((err, results) => {
+        if (err) return console.warn(err);
+        //res.cookie('data', JSON.stringify(results));
+        res.send(results);
+    });
+});
 
 app.get('*', (req, res) => {
     if (db) {
         db.collection('electric_analytics').find({}).toArray((err, results) => {
             if (err) return console.warn(err);
-            res.sendFile(`${__dirname}/public/index.html`);
+            res.render(`${__dirname}/public/index.ejs`, {db_data: JSON.stringify(results)});
         });
     } else {
         console.warn(`NO DB DB DB!!!`);
--- electric_anal/public/cont-app.js
+++ electric_anal/public/cont-app.js
@@ -51,10 +51,13 @@ const app = angular.module('myApp', ['ngRoute', 'ngResource', 'chart.js'])
     };
     $scope.delete_id = index => $scope.identities.splice(index, 1);
 }])
-.controller("line_ctrl", function ($scope) {
-
+.controller("line_ctrl", function ($http, $scope) {
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
+  $http.get("127.0.6.1:80/my_db")
+        .then((response) => {
+            console.warn(`RESP: ${JSON.stringify(response)}`);
+        });
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
     [28, 48, 40, 19, 86, 27, 90]
@@ -82,6 +85,15 @@ const app = angular.module('myApp', ['ngRoute', 'ngResource', 'chart.js'])
     }
   };
 })
+.controller("bar_ctrl", ['$scope', function ($scope) {
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+}])
 .directive('identity', function () {
     return {
         restrict: 'E',
